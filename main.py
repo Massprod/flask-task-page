dbe0c12d7c99350939bfdfd7b16b560d4758e6a6
Pampam <piankovpe@gmail.com>
2023-04-02 18:24:16 +0600

    add login experiments

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from flask import Flask, render_template, request
+from flask_login import LoginManager, UserMixin, login_user
+from flask_sqlalchemy import SQLAlchemy
 import requests
 
 app = Flask(__name__)
+app.secret_key = "VerySecretKek!2VerySecretKek"
 
 api_base = "http://localhost:5000/"
+login_manager = LoginManager(app=app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Users(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@login_manager.user_loader
+def user_loader(id):
+    return Users.query.get(int(id))
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -18,9 +40,13 @@ def login_page():
                                     }
                               )
         print(login.json())
-        if token := login.json()["access-token"]:
-            return render_template("task/test.html")
-        return render_template("login/login.html", )
+        if login.status_code == 200:
+            token = login.json()["access_token"]
+            return render_template("task/test.html", token=token)
+        elif login.status_code == 404:
+            return render_template("login/login.html", name_inc=True)
+        elif login.status_code == 403:
+            return render_template("login/login.html", pass_inc=True)
     return render_template("login/login.html")
 
 
