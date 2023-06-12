ab45d28e0e1e047424962722e797efc747dcb661
Pampam <piankovpe@gmail.com>
2023-06-12 19:35:16 +0600

    add description, type hints

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, Response
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
 from database.models import Users
-from database.database import Base, engine, get_session
+from database.database import Base, engine, get_session, Session
 import requests
 from datetime import timedelta, datetime
 import json
-
 
 api_base = "http://localhost:5000/"
 
@@ -76,26 +75,33 @@ def login_page():
 
 @app.route("/register", methods=["GET", "POST"])
 def register_page():
+    """
+    Adding new users into front/back db, if Username isn't already taken.
+        regex for -> Username == '^[A-Za-z'\'d]{2,50}$' | limited to 50chars
+                                          !^remove brackets
+        regex for -> Password == '^[A-Za-z'\'d@$!%*#?&]{8,100}$'
+                                          !^remove brackets
+    """
     if current_user.is_authenticated:
         return redirect(url_for("task_page"))
     elif request.method == "POST":
-        db = next(get_session())
-        data = request.form
-        register = requests.post(f"{api_base}user/new",
-                                 json={"login": data["username"],
-                                       "password": data["password"],
-                                       }
-                                 )
+        db: Session = next(get_session())
+        data: json = request.form
+        register: Response = requests.post(f"{api_base}user/new",
+                                           json={"login": data["username"],
+                                                 "password": data["password"],
+                                                 }
+                                           )
         if register.status_code == 200:
-            registered = register.json()
-            new_user = Users(id=registered["id"],
-                             login=registered["login"],
-                             )
+            registered: json = register.json()
+            new_user: Users = Users(id=registered["id"],
+                                    login=registered["login"],
+                                    )
             db.add(new_user)
             db.commit()
             return redirect(url_for("login_page"))
         elif register.status_code == 403:
-            return render_template("login/register.html", taken=data["username"], copyright=copy_year)
+            return render_template("login/register.html", taken=data["username"], copyright=copy_year), 403
     return render_template("login/register.html", copyright=copy_year)
 
 
